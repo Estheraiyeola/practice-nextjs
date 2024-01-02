bf67078957b2c33f7318d0cef979eb7bc8de5f13
Estheraiyeola <estheraiyeola@yahoo.com>
2024-01-02 23:11:57 +0100

    Added files on next.js

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,36 @@
+// links
+import Link from "next/link";
+
+//icons
+import{ RiInstagramLine,
+        RiGithubLine, 
+        RiMailLine,
+        RiDownloadLine} from 'react-icons/ri'
+import { SiLinkedin, SiMedium, SiOpencv } from "react-icons/si";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiMailLine/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <SiLinkedin/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <SiMedium/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiDownloadLine/>
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
